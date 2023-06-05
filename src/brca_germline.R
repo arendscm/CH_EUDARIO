1959f788b204f580ceab4103009a2d31ed1885a5
klara2299 <k99.kopp@gmail.com>
2023-06-05 15:48:48 +0200

    ids Sample information with collection date
    BRCA also for SC candidates

--- src/brca_germline.R
+++ src/brca_germline.R
@@ -86,3 +86,35 @@ save.image("data/interim/brca.RData")
 
 filename="output/BRCA_and_HRD_GermlineStatus.xlsx"
 write.xlsx(df.hrd_germline,filename,sheetName="HRD", append=TRUE)
+
+
+#### BRCA Status for SC Candidates ####
+df->df.orig
+df.orig %>% filter(is.na(Patient.ID)) -> df
+
+df %>% 
+  filter(Gene == "BRCA1"|Gene =="BRCA2") %>%
+  filter(TVAF > 0.25) %>%
+  filter(ExonicFunc!= "synonymous SNV") %>%
+  filter(AF < 0.05)%>%
+  left_join(., brcaexchange, by="Genomic_Coordinate_hg38") %>%
+  filter(is.element(BRCA.Exchange_Pathogenicity_expert,c("Pathogenic","Not Yet Reviewed"))|
+           (is.na(BRCA.Exchange_Pathogenicity_expert)&
+              (is.element(ExonicFunc,c("frameshift substitution","stopgain"))|
+                 is.element(Func,c("splicing","exonic;splicing")))))%>%## all variants that are classified as pathogenic by expert panel or not yet reviewed, or that have no match in BRCA exchange but are truncating
+  filter(!str_detect(BRCA.Exchange_Clinical_Significance_ClinVar,"Benign")|is.na(BRCA.Exchange_Clinical_Significance_ClinVar))-> df.brca_germline_SC
+
+##identify other HRD Gene germline mutations
+hrd_genes <- c("ATM","ATR","BARD1","BRIP1","CDK12","CHEK1","CHEK2","EMSY","FAM175A","FANCA","FANCC","FANCI","FANCL","MLH1","MRE11","MSH2","MSH6","NBN","PALB2","PMS2","RAD21","RAD50","RAD51","RAD51C","RAD51D","RAD52","RAD54L","PTEN","BRCC3")
+
+df %>% 
+  filter(is.element(Gene,hrd_genes)) %>%
+  filter(TVAF > 0.25) %>%
+  filter(ExonicFunc!= "synonymous SNV") %>%
+  filter(Func == "exonic")%>%
+  filter(AF < 0.01) %>%
+  filter(is.element(ExonicFunc,c("frameshift substitution","stopgain"))) -> df.hrd_germline_SC # list has to be discussed with an expert
+
+filename="output/SC-BRCAandHRD-Status.xlsx"
+write.xlsx(df.brca_germline_SC,filename,sheetName="BRCA",append=TRUE)
+write.xlsx(df.hrd_germline_SC,filename, sheetName="HRD",append=TRUE)
